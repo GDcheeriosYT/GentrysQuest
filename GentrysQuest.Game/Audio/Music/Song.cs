7d60948b7fc02f532f72fc1b3810b15abd4ce4ce
GDcheeriosYT <minecreeper0913@gmail.com>
2023-12-11 03:09:11 -0600

    cfg commit

---
+++ GentrysQuest.Game/Audio/Music/Song.cs
@@ -0,0 +1,17 @@
+using osu.Framework.Graphics.Audio;
+
+namespace GentrysQuest.Game.Audio.Music
+{
+    public class Song
+    {
+        private DrawableTrack song;
+        private TimingPointsHandler timingPointsHandler;
+    }
+}
+
+// These are quotes my fellow co workers made on this file.
+
+// "benis benis bines" - Payton Schutz
+// "my meat is super big= 1v2" - Vitalijs Cameron
+// "dont listen to vitsljis, hes got a 3.5 incher" - Payton Schutz
+// "Elijah like CBT maximum pain" - Ellijah F (Secret last name)
