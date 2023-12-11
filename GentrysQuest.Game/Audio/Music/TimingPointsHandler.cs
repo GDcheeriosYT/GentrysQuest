23239d82ad0396086aa7b7e7f1508b8150c62fdb
GDcheeriosYT <minecreeper0913@gmail.com>
2023-12-10 21:57:12 -0600

    set up base song files

---
+++ GentrysQuest.Game/Audio/Music/TimingPointsHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace GentrysQuest.Game.Audio.Music
+{
+    public class TimingPointsHandler
+    {
+        private List<float> timingPoints;
+
+        public TimingPointsHandler(List<float> timingPoints)
+        {
+            this.timingPoints = timingPoints;
+        }
+    }
+}
