f5097b3d683744c0bc45629c047f276eb22c71d6
GDcheerios <minecreeper0913@gmail.com>
2024-05-29 17:42:09 -0500

    can't forget the quotes

--- GentrysQuest.Game/Audio/Music/ISong.cs
+++ GentrysQuest.Game/Audio/Music/ISong.cs
@@ -7,3 +7,10 @@ namespace GentrysQuest.Game.Audio.Music
         TimingPointsHandler TimingPoints { get; }
     }
 }
+
+// These are quotes my fellow co-workers made on this file.
+
+// "benis benis bines" - Payton Schutz
+// "my meat is super big= 1v2" - Vitalijs Cameron
+// "dont listen to vitsljis, hes got a 3.5 incher" - Payton Schutz
+// "Elijah like CBT maximum pain" - Ellijah F (Secret last name)
