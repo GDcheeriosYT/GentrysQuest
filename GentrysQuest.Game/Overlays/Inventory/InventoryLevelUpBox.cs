c7a5c7c25acee78bcd84106e5d3cf85ee1864373
GDcheerios <minecreeper0913@gmail.com>
2024-06-18 09:00:19 -0500

    improved levelup box

--- GentrysQuest.Game/Overlays/Inventory/InventoryLevelUpBox.cs
+++ GentrysQuest.Game/Overlays/Inventory/InventoryLevelUpBox.cs
@@ -1,29 +1,70 @@
 using GentrysQuest.Game.Database;
 using osu.Framework.Graphics;
+using osu.Framework.Input.Events;
 using osuTK.Input;
 
 namespace GentrysQuest.Game.Overlays.Inventory
 {
-    public partial class InventoryLevelUpBox : InventoryButton
+    public partial class InventoryLevelUpBox() : InventoryButton("$0")
     {
         private int amount;
+        private bool multiplyTen = false;
+        private bool multiplyHundred = false;
 
-        public InventoryLevelUpBox()
-            : base("$0")
+        protected override bool OnMouseDown(MouseDownEvent e)
         {
-            SetAction(delegate
-            {
-                int actionAmount = 1;
-                if (Keyboard.GetState().IsKeyDown(Key.ControlLeft)) actionAmount *= 10;
-                if (Keyboard.GetState().IsKeyDown(Key.ShiftLeft)) actionAmount *= 100;
-                if (Keyboard.GetState().IsKeyDown(Key.AltLeft)) actionAmount = -actionAmount;
+            int actionAmount = 1;
+            if (multiplyTen) actionAmount *= 10;
+            if (multiplyHundred) actionAmount *= 100;
 
-                if (amount + actionAmount >= 0) amount += actionAmount;
-                else amount = 0;
-                SetTextColor(Colour4.Red);
-                if (GameData.Money.CanAfford(amount)) SetTextColor(Colour4.Black);
-                SetText($"${amount.ToString()}");
-            });
+            switch (e.Button)
+            {
+                case MouseButton.Left:
+                    break;
+
+                case MouseButton.Right:
+                    actionAmount = -actionAmount;
+                    break;
+            }
+
+            if (amount + actionAmount >= 0) amount += actionAmount;
+            else amount = 0;
+            SetTextColor(Colour4.Red);
+            if (GameData.Money.CanAfford(amount)) SetTextColor(Colour4.Black);
+            SetText($"${amount.ToString()}");
+            return base.OnMouseDown(e);
+        }
+
+        protected override bool OnKeyDown(KeyDownEvent e)
+        {
+            switch (e.Key)
+            {
+                case Key.ControlLeft:
+                    multiplyTen = true;
+                    break;
+
+                case Key.ShiftLeft:
+                    multiplyHundred = true;
+                    break;
+            }
+
+            return base.OnKeyDown(e);
+        }
+
+        protected override void OnKeyUp(KeyUpEvent e)
+        {
+            switch (e.Key)
+            {
+                case Key.ControlLeft:
+                    multiplyTen = false;
+                    break;
+
+                case Key.ShiftLeft:
+                    multiplyHundred = false;
+                    break;
+            }
+
+            base.OnKeyUp(e);
         }
 
         public int GetAmount() => amount;
