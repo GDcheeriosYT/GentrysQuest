221459a560f25051f9c99e0ef19dc2f2effe526e
GDcheeriosYT <minecreeper0913@gmail.com>
2024-07-29 04:05:46 -0500

    fix doc

--- GentrysQuest.Game/Content/Families/JVee/ElHefe.cs
+++ GentrysQuest.Game/Content/Families/JVee/ElHefe.cs
@@ -1,17 +1,3 @@
-// * Name              : GentrysQuest.Game
-//  * Author           : Brayden J Messerschmidt
-//  * Created          : 07/29/2024
-//  * Course           : CIS 169 C#
-//  * Version          : 1.0
-//  * OS               : Windows 11 22H2
-//  * IDE              : Jet Brains Rider 2023
-//  * Copyright        : This is my work.
-//  * Description      : desc.
-//  * Academic Honesty : I attest that this is my original work.
-//  * I have not used unauthorized source code, either modified or
-//  * unmodified. I have not given other fellow student(s) access
-//  * to my program.
-
 using System.Collections.Generic;
 using GentrysQuest.Game.Entity;
 
